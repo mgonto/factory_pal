0646315e3dc57829ef5575661d9e6f1c3fb25206
Martin Gontovnikas <martin@gonto.com.ar>
2013-01-14 22:54:09 -0300

    Updated sample for 0.2 release

--- samples/project/Build.scala
+++ samples/project/Build.scala
@@ -3,29 +3,26 @@ import sbt.Keys._
 
 object ApplicationBuild extends Build {
 
-
   lazy val root = Project(
     id = "factory_pal_sample",
     base = file("."),
     settings = Project.defaultSettings ++ Seq(
       name := "factory_pal_sample",
       organization := "ar.com.gonto",
-      version := "0.1",
-      scalaVersion := "2.10.0-RC3",
+      version := "0.2",
+      scalaVersion := "2.10.0",
       scalacOptions += "",
       licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
       libraryDependencies ++= Seq(
-       "org.scala-lang" % "scala-compiler" % "2.10.0-RC3",
-       "ar.com.gonto" % "factory_pal_2.10" % "0.1.1-SNAPSHOT",
-       "org.scalatest" % "scalatest_2.10.0-RC3" % "1.8-B1" % "test"
+       "org.scala-lang" % "scala-compiler" % "2.10.0",
+       "ar.com.gonto" % "factory_pal_2.10" % "0.2",
+       "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
       ),
       resolvers ++= Seq(
          "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
          Resolver.url("Factory Pal Repository", 
-          url("http://mgonto.github.com/snapshots/"))(Resolver.ivyStylePatterns)
+          url("http://mgonto.github.com/releases/"))(Resolver.ivyStylePatterns)
       )
-
-      // add other settings here
     )
   )
 }
