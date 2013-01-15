36ef2f55e7501ee0c0edf6f511c53b95f1d900db
Martin Gontovnikas <martin@gonto.com.ar>
2013-01-15 18:07:11 -0300

    Updated version for 0.2.1 release

--- samples/project/Build.scala
+++ samples/project/Build.scala
@@ -9,13 +9,13 @@ object ApplicationBuild extends Build {
     settings = Project.defaultSettings ++ Seq(
       name := "factory_pal_sample",
       organization := "ar.com.gonto",
-      version := "0.2",
+      version := "0.2.1",
       scalaVersion := "2.10.0",
       scalacOptions += "",
       licenses      := ("Apache2", new java.net.URL("http://www.apache.org/licenses/LICENSE-2.0.txt")) :: Nil,
       libraryDependencies ++= Seq(
        "org.scala-lang" % "scala-compiler" % "2.10.0",
-       "ar.com.gonto" % "factory_pal_2.10" % "0.2",
+       "ar.com.gonto" % "factory_pal_2.10" % "0.2.1",
        "org.scalatest" % "scalatest_2.10" % "1.9.1" % "test"
       ),
       resolvers ++= Seq(
