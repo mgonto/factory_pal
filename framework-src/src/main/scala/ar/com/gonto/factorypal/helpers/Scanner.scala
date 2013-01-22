ca69ca06489d34fb8987b6bfa91884165e2482fd
Tim Steinbach <steinbach.tim@googlemail.com>
2013-01-22 11:24:34 -0500

    Format Scanner

--- framework-src/src/main/scala/ar/com/gonto/factorypal/helpers/Scanner.scala
+++ framework-src/src/main/scala/ar/com/gonto/factorypal/helpers/Scanner.scala
@@ -4,6 +4,7 @@ import scala.language.experimental.macros
 import scala.reflect.macros.Context
 
 object Scanner {
+  
   def subclasses[A]: Set[A] = macro subclasses_impl[A]
 
   def subclasses_impl[A: c.WeakTypeTag](c: Context) = {
@@ -18,5 +19,6 @@ object Scanner {
       }
 
       c.Expr[Set[A]](Apply(Select(reify(Set).tree, "apply"), children.map(New(_))))
-    }
   }
+  
+}
